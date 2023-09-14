9c0655e9536fe2b5460ca1d17532271d90a757f3
Najrul <mdnajrulislam.4738@gmail.com>
2023-09-14 21:20:44 +0600

    I have made Header component into components file and installed Tailwind css and daisyUI in this file. I have also added Inter font in this projects.

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <div className="w-full pt-12 pb-8">
+      <h1 className="text-4xl text-center">Course Registration</h1>
+    </div>
+  );
+};
+
+export default Header;
